540a69e60ae51d1ab1638e30a254580ce9667a11
Daniel Zahariev <daniel.zahariev@gmail.com>
2014-01-15 12:28:11 +0200

    Fix for overlapping AJAX requests

--- assets/example2.js
+++ assets/example2.js
@@ -1,10 +1,14 @@
 $(function () {
+    var ajax_request = false;
     $('textarea.tagged_text_ex2').textntags({
         onDataRequest:function (mode, query, triggerChar, callback) {
-            $.getJSON('assets/data.json', function(responseData) {
+            // fix for overlapping requests
+            if(ajax_request) ajax_request.abort();
+            ajax_request = $.getJSON('assets/data.json', function(responseData) {
                 query = query.toLowerCase();
                 responseData = _.filter(responseData, function(item) { return item.name.toLowerCase().indexOf(query) > -1; });
                 callback.call(this, responseData);
+                ajax_request = false;
             });
         }
     });
